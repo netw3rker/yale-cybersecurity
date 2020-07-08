8c4d2014cca1fc881b56d2ffb870811356abc89b
Evan Willhite <evan@fourkitchens.com>
2020-07-08 16:10:56 -0500

    YI-33: medium, large screens

--- web/themes/custom/yale_infosec/components/03-organisms/site/site.stories.js
+++ web/themes/custom/yale_infosec/components/03-organisms/site/site.stories.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect } from '@storybook/client-api';
 
 import footerTwig from './site-footer/site-footer.twig';
 import siteHeader from './site-header/site-header.twig';
@@ -7,7 +6,7 @@ import siteHeader from './site-header/site-header.twig';
 import footerSocial from '../../02-molecules/menus/social/social-menu.yml';
 import footerMenu from '../../02-molecules/menus/inline/inline-menu.yml';
 import breadcrumbData from '../../02-molecules/menus/breadcrumbs/breadcrumbs.yml';
-import mainMenuData from '../../02-molecules/menus/main-menu/main-menu.yml';
+import headerMenuData from './site-header/site-header.yml';
 
 import '../../02-molecules/menus/main-menu/main-menu';
 
@@ -23,16 +22,13 @@ export const footer = () => (
     }}
   />
 );
-export const header = () => {
-  useEffect(() => Drupal.attachBehaviors(), []);
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: siteHeader({
-          ...breadcrumbData,
-          ...mainMenuData,
-        }),
-      }}
-    />
-  );
-};
+export const header = () => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html: siteHeader({
+        ...breadcrumbData,
+        ...headerMenuData,
+      }),
+    }}
+  />
+);
