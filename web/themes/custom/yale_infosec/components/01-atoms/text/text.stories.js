16312bc8c0c4216aac6c721adabcf576c3419e64
Evan Willhite <evan@fourkitchens.com>
2020-07-08 11:34:53 -0500

    YI-45: text, link, list styling, example content

--- web/themes/custom/yale_infosec/components/01-atoms/text/text.stories.js
+++ web/themes/custom/yale_infosec/components/01-atoms/text/text.stories.js
@@ -4,9 +4,11 @@ import heading from './headings/_heading.twig';
 import blockquote from './text/02-blockquote.twig';
 import pre from './text/05-pre.twig';
 import paragraph from './text/03-inline-elements.twig';
+import exampleContentTwig from './text/04-example-content.twig';
 
 import blockquoteData from './text/blockquote.yml';
 import headingData from './headings/headings.yml';
+import exampleData from './text/example-content.yml';
 
 /**
  * Storybook Definition.
@@ -20,11 +22,14 @@ export const headingsExamples = () => (
   <div dangerouslySetInnerHTML={{ __html: headings }} />
 );
 export const blockquoteExample = () => (
-  <div dangerouslySetInnerHTML={{ __html: blockquote(blockquoteData) }} />
+  <div dangerouslySetInnerHTML={{ __html: blockquote({ ...blockquoteData, blockquote_modifiers: ['sb'] }) }} />
 );
 export const preformatted = () => (
   <div dangerouslySetInnerHTML={{ __html: pre({}) }} />
 );
 export const random = () => (
   <div dangerouslySetInnerHTML={{ __html: paragraph({}) }} />
+);
+export const exampleContent = () => (
+  <div dangerouslySetInnerHTML={{ __html: exampleContentTwig(exampleData) }} />
 );
