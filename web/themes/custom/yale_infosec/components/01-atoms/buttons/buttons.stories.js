0575e48375605e774ea0388df840da82d02941b0
Evan Willhite <evan@fourkitchens.com>
2020-07-16 12:21:31 -0500

    YI-37: button color options, mixin

--- web/themes/custom/yale_infosec/components/01-atoms/buttons/buttons.stories.js
+++ web/themes/custom/yale_infosec/components/01-atoms/buttons/buttons.stories.js
@@ -3,7 +3,6 @@ import React from 'react';
 import button from './button.twig';
 
 import buttonData from './button.yml';
-import buttonAltData from './button-alt.yml';
 
 /**
  * Storybook Definition.
@@ -11,8 +10,38 @@ import buttonAltData from './button-alt.yml';
 export default { title: 'Atoms/Buttons' };
 
 export const primary = () => (
-  <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />
+  <div dangerouslySetInnerHTML={{ __html: button({
+    ...buttonData,
+    button_modifiers: ['primary']
+  }) }} />
 );
 export const secondary = () => (
-  <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />
+  <div dangerouslySetInnerHTML={{ __html: button({
+    ...buttonData,
+    button_modifiers: ['secondary']
+  }) }} />
+);
+export const yale = () => (
+  <div dangerouslySetInnerHTML={{ __html: button({
+    ...buttonData,
+    button_modifiers: ['yale']
+  }) }} />
+);
+export const yaleLight = () => (
+  <div dangerouslySetInnerHTML={{ __html: button({
+    ...buttonData,
+    button_modifiers: ['yale-light']
+  }) }} />
+);
+export const muted = () => (
+  <div dangerouslySetInnerHTML={{ __html: button({
+    ...buttonData,
+    button_modifiers: ['muted']
+  }) }} />
+);
+export const mutedDark = () => (
+  <div dangerouslySetInnerHTML={{ __html: button({
+    ...buttonData,
+    button_modifiers: ['muted-dark']
+  }) }} />
 );
