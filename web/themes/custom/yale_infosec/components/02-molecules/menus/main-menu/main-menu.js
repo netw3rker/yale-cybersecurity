ef430bbabf083fc56ecb8b4db46db1b3403cdec1
Evan Willhite <evan@fourkitchens.com>
2020-07-21 09:06:23 -0500

    YI-27: show only current back link in mobile nav

--- web/themes/custom/yale_infosec/components/02-molecules/menus/main-menu/main-menu.js
+++ web/themes/custom/yale_infosec/components/02-molecules/menus/main-menu/main-menu.js
@@ -18,8 +18,14 @@ Drupal.behaviors.mainMenu = {
         expandMenu[i].addEventListener('click', e => {
           const menuItem = e.currentTarget;
           const subMenu = menuItem.nextElementSibling;
+          const currentMenu = document.getElementsByClassName('main-menu__child-menu--sub-current');
+
+          for (let i = 0; i < currentMenu.length; i += 1) {
+            currentMenu[i].classList.remove('main-menu__child-menu--sub-current');
+          }
 
           subMenu.classList.toggle('main-menu__child-menu--sub-open');
+          subMenu.classList.add('main-menu__child-menu--sub-current');
         });
       }
 
@@ -28,7 +34,11 @@ Drupal.behaviors.mainMenu = {
         backLinks[i].addEventListener('click', e => {
           const backLink = e.currentTarget;
           const parentMenu = backLink.parentNode;
+          const prevParentMenu = parentMenu.parentNode.parentNode.parentNode;
+
           parentMenu.classList.remove('main-menu__child-menu--sub-open');
+          parentMenu.classList.remove('main-menu__child-menu--sub-current');
+          prevParentMenu.classList.add('main-menu__child-menu--sub-current');
         });
       }
     }
