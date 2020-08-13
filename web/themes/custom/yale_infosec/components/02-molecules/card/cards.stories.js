a49c1eba309d4ac8f23c0cf4a71210391e01c4b2
Evan Willhite <evan@fourkitchens.com>
2020-08-13 14:52:40 -0500

    YI-73: date component, event card

--- web/themes/custom/yale_infosec/components/02-molecules/card/cards.stories.js
+++ web/themes/custom/yale_infosec/components/02-molecules/card/cards.stories.js
@@ -4,6 +4,8 @@ import card from './card.twig';
 
 import cardData from './card.yml';
 
+import eventData from './card-event.yml';
+
 /**
  * Storybook Definition.
  */
@@ -11,4 +13,8 @@ export default { title: 'Molecules/Cards' };
 
 export const cardExample = () => (
   <div dangerouslySetInnerHTML={{ __html: card(cardData) }} />
+);
+
+export const Event = () => (
+  <div dangerouslySetInnerHTML={{ __html: card({ ...cardData, ...eventData }) }} />
 );
