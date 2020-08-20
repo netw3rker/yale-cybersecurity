6433883dc4af901cbfc584cc76982d9d65e80dc8
Evan Willhite <evan@fourkitchens.com>
2020-08-20 12:52:52 -0500

    YI-90: alert component

---
+++ web/themes/custom/yale_infosec/components/02-molecules/alert/alert.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import alert from './alert.twig';
+
+import alertData from './alert.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Molecules|Alert' };
+
+export const Persistent = () => (
+  <div dangerouslySetInnerHTML={{ __html: alert(alertData) }} />
+);
+export const Notice = () => (
+  <div dangerouslySetInnerHTML={{ __html: alert({
+    ...alertData,
+    alert__modifiers: ['notice']
+  }) }} />
+);
+export const Warning = () => (
+  <div dangerouslySetInnerHTML={{ __html: alert({
+    ...alertData,
+    alert__modifiers: ['warning']
+  }) }} />
+);
+export const Danger = () => (
+  <div dangerouslySetInnerHTML={{ __html: alert({
+    ...alertData,
+    alert__modifiers: ['danger']
+  }) }} />
+);
+
