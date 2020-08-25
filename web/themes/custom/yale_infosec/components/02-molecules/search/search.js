8ce4bdb19548b63cb6ae5c5b3a7123ff06b5e34a
Evan Willhite <evan@fourkitchens.com>
2020-08-25 14:40:48 -0500

    YI-54: search block

--- web/themes/custom/yale_infosec/components/02-molecules/search/search.js
+++ web/themes/custom/yale_infosec/components/02-molecules/search/search.js
@@ -1,23 +1,24 @@
-Drupal.behaviors.search = {
+Drupal.behaviors.searchForm = {
   attach(context) {
     const clearSearch = context.getElementById('search-clear');
     const closeSearch = context.getElementById('search-close');
-    const search = context.getElementById('edit-search');
     
-    if (search) {
-      // Clear search text.
-      clearSearch.addEventListener('click', e => {
-        search.value = '';
-      });
+    // Clear search text.
+    clearSearch.addEventListener('click', e => {
+      const searchForm = e.currentTarget.nextElementSibling;
+      const search = searchForm.querySelectorAll('.form-text');
+      for (let i = 0; i < search.length; i += 1) {
+        search[i].value = '';
+      }
+    });
 
-      // Close Search.
-      closeSearch.addEventListener('click', e => {
-        const mainNav = context.getElementsByClassName('main-nav-search--open');
+    // Close Search.
+    closeSearch.addEventListener('click', () => {
+      const mainNav = context.getElementsByClassName('main-nav-search--open');
 
-        for (let i = 0; i < mainNav.length; i += 1) {
-          mainNav[i].classList.remove('main-nav-search--open')
-        }
-      });
-    }
+      for (let i = 0; i < mainNav.length; i += 1) {
+        mainNav[i].classList.remove('main-nav-search--open')
+      }
+    });
   },
 };
